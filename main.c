3af9281959faee692f4818dce3a70b4bfafe812f
Paweł Wiśniewski <pawel.wis93@gmail.com>
2015-12-24 13:20:28 +0100

    Przebudowa MENU przy użyciu struktury Maps. Usunięcie funkcji nad którymi nie pracuję (YAGNI)

--- main.c
+++ main.c
@@ -3,47 +3,42 @@
 #include"life.h"
 
 int main(int argc, char *argv[]){
-	GtkWidget *window;
-	GtkWidget *map, *menu, *menu_polozenie;
-	GtkWidget *spiece_choose, *spiece_name, *plant, *herbivore, *carnivore;
-	GtkWidget *size, *map_size, *start_button, *stop_button;
-	/*initializing window*/
+	Maps *map;
+	map = g_malloc ( sizeof( Maps ) );
+
 	gtk_init (&argc, &argv);
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_resize(GTK_WINDOW(window), 800, 640);
-	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-	gtk_window_set_title (GTK_WINDOW(window), "Small Game of Evolution");
+
+	map->window = g_object_new( GTK_TYPE_WINDOW, "window-position", GTK_WIN_POS_CENTER,
+	"default-width", 800, "default-height", 640, "title", "Small Game of Evolution", "border-width", 5, NULL );
 	/*Generating map*/
-		
 
 	/*Creating Menu*/
-		menu_polozenie = gtk_alignment_new( 1, 0.25, 0.1, 0.1 );
-		menu = gtk_vbox_new(FALSE, 1);
-		spiece_name = gtk_label_new("Choose the spieces:");
-		gtk_container_add(GTK_CONTAINER(menu), spiece_name);
-		spiece_choose = gtk_combo_box_new_text();
-		gtk_combo_box_append_text(GTK_COMBO_BOX(spiece_choose), "PLANT");
-		gtk_combo_box_append_text(GTK_COMBO_BOX(spiece_choose), "HERBIVORE");
-		gtk_combo_box_append_text(GTK_COMBO_BOX(spiece_choose), "CARNIVORE");
-		gtk_container_add(GTK_CONTAINER(menu), spiece_choose);
-		map_size = gtk_label_new("Map size:");
-		gtk_container_add(GTK_CONTAINER(menu), map_size);
-		size = gtk_combo_box_new_text();
-		gtk_combo_box_append_text(GTK_COMBO_BOX(size), "100x100");
-		gtk_combo_box_append_text(GTK_COMBO_BOX(size), "200x200");
-		gtk_combo_box_append_text(GTK_COMBO_BOX(size), "300x300");
-		gtk_container_add(GTK_CONTAINER(menu), size);
-		start_button = gtk_button_new_with_label("START");
-		stop_button = gtk_button_new_with_label("STOP");
-		gtk_box_pack_start (GTK_BOX(menu), start_button, TRUE, TRUE, 0);
-		gtk_box_pack_start (GTK_BOX(menu), stop_button, TRUE, TRUE, 0);
-		gtk_container_add(GTK_CONTAINER(window), menu_polozenie);
-		gtk_container_add(GTK_CONTAINER(menu_polozenie), menu);
-	
+		map->menu_polozenie = gtk_alignment_new( 1, 0.25, 0.1, 0.1 );
+		map->menu = gtk_vbox_new(FALSE, 1);
+		map->spiece_name = gtk_label_new("Choose the spieces:");
+		gtk_container_add(GTK_CONTAINER(map->menu), map->spiece_name);
+		map->spiece_choose = gtk_combo_box_new_text();
+		gtk_combo_box_append_text(GTK_COMBO_BOX(map->spiece_choose), "PLANT");
+		gtk_combo_box_append_text(GTK_COMBO_BOX(map->spiece_choose), "HERBIVORE");
+		gtk_combo_box_append_text(GTK_COMBO_BOX(map->spiece_choose), "CARNIVORE");
+		gtk_container_add(GTK_CONTAINER(map->menu), map->spiece_choose);
+		map->map_size = gtk_label_new("Map size:");
+		gtk_container_add(GTK_CONTAINER(map->menu), map->map_size);
+		map->size = gtk_combo_box_new_text();
+		gtk_combo_box_append_text(GTK_COMBO_BOX(map->size), "100x100");
+		gtk_combo_box_append_text(GTK_COMBO_BOX(map->size), "200x200");
+		gtk_combo_box_append_text(GTK_COMBO_BOX(map->size), "300x300");
+		gtk_container_add(GTK_CONTAINER(map->menu), map->size);
+		map->start_button = g_object_new( GTK_TYPE_TOGGLE_BUTTON, "label", "START", NULL );
+		map->stop_button = g_object_new( GTK_TYPE_BUTTON, "label", "STOP", NULL);
+		gtk_box_pack_start (GTK_BOX(map->menu), map->start_button, TRUE, TRUE, 0);
+		gtk_box_pack_start (GTK_BOX(map->menu), map->stop_button, TRUE, TRUE, 0);
+		gtk_container_add(GTK_CONTAINER(map->window), map->menu_polozenie);
+		gtk_container_add(GTK_CONTAINER(map->menu_polozenie), map->menu);
 
 
-	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	gtk_widget_show_all (window);
+	g_signal_connect(G_OBJECT(map->window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show_all (map->window);
 	gtk_main ();
 return 0;
 }
