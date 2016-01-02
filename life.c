03ab1d933f666f52295ab068fc25740ea199a6ec
Paweł Wiśniewski <pawel.wis93@gmail.com>
2016-01-02 14:41:16 +0100

    added game screen

--- life.c
+++ life.c
@@ -32,10 +32,12 @@ int i=0;
 
 void printmap(Maps *maps){
 int i=0, x, y;
-	for(x=0; x<=maps->n_xy; x++){
-		for(y=0; y<=maps->n_xy; y++){
+	for(x=0; x<maps->n_xy; x++){
+		for(y=0; y<maps->n_xy; y++){
 			if(maps->map[i] == 'l'){
 			maps->game_creat = gtk_image_new_from_file("img/land.png");
+			gtk_widget_set_size_request(maps->game_creat, 2, 2);
+			gtk_fixed_put(GTK_FIXED(maps->game_window), maps->game_creat, 20+2*x, 100+2*y);
 			i++;
 			}
 			}
--- main.c
+++ main.c
@@ -14,42 +14,43 @@ int main(int argc, char *argv[]){
 
 	map->window = g_object_new( GTK_TYPE_WINDOW, "window-position", GTK_WIN_POS_CENTER,
 	"default-width", 800, "default-height", 640, "title", "Small Game of Evolution", "border-width", 5, NULL );
+	/*Creating Menu*/
+		map->game_window = gtk_fixed_new(); 
+		gtk_container_add(GTK_CONTAINER(map->window), map->game_window);
+
 	/*Generating map*/
 	create_map(map);
 
-	/*Creating Menu*/
-		map->menu = gtk_fixed_new(); 
-		gtk_container_add(GTK_CONTAINER(map->window), map->menu);
 
 		/*SPIECES CHOOSE PART*/
 		map->labels = gtk_label_new("SPIECES"); 
-		gtk_fixed_put(GTK_FIXED(map->menu), map->labels, 610, 50);
+		gtk_fixed_put(GTK_FIXED(map->game_window), map->labels, 610, 50);
 		map->spiece_choose = gtk_combo_box_new_text(); 
 		gtk_combo_box_append_text(GTK_COMBO_BOX(map->spiece_choose), "PLANT");
 		gtk_combo_box_append_text(GTK_COMBO_BOX(map->spiece_choose), "HERBIVORE");
 		gtk_combo_box_append_text(GTK_COMBO_BOX(map->spiece_choose), "CARNIVORE");
 		gtk_widget_set_size_request(map->spiece_choose, 100, 50);
-		gtk_fixed_put(GTK_FIXED(map->menu), map->spiece_choose, 600, 70);
+		gtk_fixed_put(GTK_FIXED(map->game_window), map->spiece_choose, 600, 70);
 
 		/*MAP SIZE CHOOSE PART*/
 		map->labels = gtk_label_new("MAP SIZE"); 
-		gtk_fixed_put(GTK_FIXED(map->menu), map->labels, 610, 150);
+		gtk_fixed_put(GTK_FIXED(map->game_window), map->labels, 610, 150);
 		map->size = gtk_combo_box_new_text(); 
 		gtk_widget_set_size_request(map->size, 100, 50);
 		gtk_combo_box_append_text(GTK_COMBO_BOX(map->size), "100x100");
 		gtk_combo_box_append_text(GTK_COMBO_BOX(map->size), "200x200");
-		gtk_fixed_put(GTK_FIXED(map->menu), map->size, 600, 170);
+		gtk_fixed_put(GTK_FIXED(map->game_window), map->size, 600, 170);
 			g_signal_connect(G_OBJECT(map->size), "changed", G_CALLBACK(resize), (gpointer)map);
 		
 		/*START/PAUSE BUTTON PART*/
 		map->start_button = gtk_toggle_button_new_with_label("START"); 
 		gtk_widget_set_size_request(map->start_button, 150, 50);
-		gtk_fixed_put(GTK_FIXED(map->menu), map->start_button, 600, 300);
+		gtk_fixed_put(GTK_FIXED(map->game_window), map->start_button, 600, 300);
 		
 		/*STOP BUTTON PART*/
 		map->stop_button = gtk_button_new_with_label("STOP"); 
 		gtk_widget_set_size_request(map->stop_button, 150, 50);
-		gtk_fixed_put(GTK_FIXED(map->menu), map->stop_button, 600, 370);
+		gtk_fixed_put(GTK_FIXED(map->game_window), map->stop_button, 600, 370);
 			
 
 	
